ba85845be49e595762c72193b2c346a43d4d9c84
Matthew Franglen <matthew@franglen.org>
2017-03-20 22:04:14 +0000

    Extending Array breaks everything

--- __tests__/code.spec.js
+++ __tests__/code.spec.js
@@ -1,9 +1,24 @@
 import { ExampleCode } from '../src/code.js';
 
-describe('hello function', () => {
-  it('should return the traditional greeting', () => {
-    const v = new ExampleCode().hello();
+describe('array', () => {
+  it('should return the traditional greeting, when patched', () => {
+    const v = new Array(1, 2, 3);
+    v.hello = () => "Hello, World!";
 
-    expect(v).toEqual("Hello, World!");
+    expect(v.hello()).toEqual("Hello, World!");
+  });
+});
+
+describe('example code', () => {
+  it('should be constructable', () => {
+    const v = new ExampleCode(1, 2, 3);
+
+    expect('' + v).toEqual('1,2,3');
+  });
+
+  it('should return the traditional greeting', () => {
+    const v = new ExampleCode(1, 2, 3);
+
+    expect(v.hello()).toEqual("Hello, World!");
   });
 });
--- src/code.js
+++ src/code.js
@@ -1,4 +1,4 @@
-class ExampleCode {
+class ExampleCode extends Array {
 
   hello() {
     return "Hello, World!";
